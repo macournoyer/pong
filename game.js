64450684ea23304feeb7ccffab8dfdf18d8c59de
Garrett Martin <garrettqmartin8@optimum.net>
2014-03-02 17:30:57 -0600

    add initial game loop, update, and draw functions

--- game.js
+++ game.js
@@ -34,3 +34,28 @@ Game.keys = {
   39: 'right',
   40: 'down'
 }
+
+Game.prototype.start = function(){
+  var self = this,
+      fps = 100,
+      interval = 1000 / fps
+  
+  setInterval(function(){
+    self.update()
+    self.draw ()
+  }, interval)
+}
+
+Game.prototype.update = function(){
+  this.entities.forEach(function(entity){
+    if (entity.update) entity.update()
+  }) 
+}
+
+Game.prototype.draw = function(){
+  var self = this
+
+  this.entities.forEach(function(entity){
+    if (entity.draw) entity.draw(self.context)
+  }) 
+}
