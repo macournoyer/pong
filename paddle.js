87914c72a720a1932614e983a70f94061e61a4fe
Garrett Martin <garrettqmartin8@optimum.net>
2014-03-02 20:54:01 -0600

    moving paddle with vertical boundaries

--- paddle.js
+++ paddle.js
@@ -10,3 +10,22 @@ var Paddle = function() {
 
 Paddle.prototype = new Entity()
 Paddle.prototype.constructor = Paddle
+
+Paddle.prototype.update = function(){
+  Entity.prototype.update.apply(this, arguments)
+  var speed = 15
+
+  if (game.keyPressed.up) {
+    this.yVelocity = -speed
+  } else if (game.keyPressed.down) {
+    this.yVelocity = speed
+  } else {
+    this.yVelocity = 0
+  }
+
+  if(this.y > game.height || this.y < 0) {
+    this.yVelocity = 0
+  }
+
+  this.y = Math.min(Math.max(this.y, 0), game.height - this.height)
+}
